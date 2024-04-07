a4ba40ce479b71a61477de7586114f35a3e707d0
VADUNG <vad24042002@gmail.com>
2024-04-07 12:22:29 +0700

    Create pages and routes

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+
+export default function Header() {
+  return <div></div>;
+}
