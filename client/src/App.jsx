0bd922a461d790b469afa1399740cf38fe6c765e
VADUNG <vad24042002@gmail.com>
2024-04-10 14:30:04 +0700

    Complete create a post page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,10 @@ import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+
+import CreatePost from "./pages/CreatePost";
+//import UpdatePost from "./pages/UpdatePost";
 
 export default function App() {
   return (
@@ -21,6 +25,9 @@ export default function App() {
         <Route path="/project" element={<Projects />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />} />
         </Route>
       </Routes>
       <Footer />
