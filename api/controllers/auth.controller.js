fc90fe6e63c247857398def3c7ca8ce193daa6c2
VADUNG <vad24042002@gmail.com>
2024-04-12 09:56:14 +0700

    Add dashboard overview to the admin dashboard

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -94,7 +94,8 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign(
         { id: newUser._id, isAdmin: newUser.isAdmin },
-        process.env.JWT_SECRET
+        process.env.JWT_SECRET,
+        { expiresIn: "2d" }
       );
       const { password, ...rest } = newUser._doc;
       res
