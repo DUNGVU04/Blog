118c84d7290030f95da62e8ab558f1d7c0f18e09
VADUNG <vad24042002@gmail.com>
2024-04-16 14:45:11 +0700

    update api

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Server is running on port 3000!");
 });
 
